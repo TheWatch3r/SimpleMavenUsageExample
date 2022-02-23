99bea82adaf51cdd7aa4c3749f5280f6a63c573f
35s3155 <35s3155@bztf.ch>
2022-02-23 13:30:27 +0100

    Reformated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.apache.log4j.Logger;
+
 import java.io.*;
 import java.sql.SQLException;
 
@@ -7,7 +8,7 @@ public class Main {
     static Logger log = Logger.getLogger(Main.class.getName());
     static int number_of_values = 30;
 
-    public static void main(String[] args)throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException {
         Fibonacci result = new Fibonacci(number_of_values);
         log.debug("Hello this is a debug message");
         log.info(Fibonacci.calculate());
