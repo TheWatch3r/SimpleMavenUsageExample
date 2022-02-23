869958718fc7bd2bac9a12a4436525c79da9a854
35s3155 <35s3155@bztf.ch>
2022-02-23 13:27:34 +0100

    Fibonacci in Logger ausgegeben

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,11 @@ import java.sql.SQLException;
 public class Main {
 
     static Logger log = Logger.getLogger(Main.class.getName());
+    static int number_of_values = 30;
 
     public static void main(String[] args)throws IOException, SQLException {
+        Fibonacci result = new Fibonacci(number_of_values);
         log.debug("Hello this is a debug message");
-        log.info("Hello this is an info message");
+        log.info(Fibonacci.calculate());
     }
 }
